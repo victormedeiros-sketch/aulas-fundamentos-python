5461a465f1b25dcaaa79397db70d331f741aebbf
victormedeiros-sketch <victorodilonjc@gmail.com>
2025-11-19 17:27:14 +0000

    novas correções
    ex061 e ex062

---
+++ Exercicios/ex061.py
@@ -0,0 +1,18 @@
+# Crie um programa que crie uma matriz 3x3 e preencha com valores lidos pelo teclado.
+# No final mostre a matriz com a formatação adequada
+
+numeros = list()
+for linha in range(0,3):
+    temp = list()
+
+    for coluna in range(0,3):
+        num = int(input('Digite um número: '))
+        temp.append(num)
+
+    numeros.append(temp[:])
+
+for lista in numeros:
+    for valor in lista:
+        print(valor, end=' ')
+    print()
+
---
+++ Exercicios/ex062.py
@@ -0,0 +1,34 @@
+#Melhore o exercicio 61
+#
+
+
+
+numeros = list()
+soma_valores_pares = 0
+soma_segunda_coluna = 0
+maior_teerceira_linha = ''
+
+for linha in range(0,3):
+    temp = list()
+
+    for coluna in range(0,3):
+        num = int(input('Digite um número: '))
+        if num % 2 == 0:
+            soma_valores_pares += num
+        if coluna == 1:
+            soma_segunda_coluna += num
+
+        if linha == 2:
+            if coluna == 0:
+                maior_terceira_linha = num
+            else:
+                if num > maior_terceira_linha:
+                    maior_terceira_linha = num
+        temp.append(num)
+
+    numeros.append(temp[:])
+
+for lista in numeros:
+    for valor in lista:
+        print(valor, end=' ')
+    print()
