8cc9816037264a76671e5184a7968f1584f38533
victormedeiros-sketch <victorodilonjc@gmail.com>
2026-01-06 19:32:41 +0000

    ex_078

--- Exercicios/ex078/ex078.py
+++ Exercicios/ex078/ex078.py
@@ -1,30 +1,26 @@
-# Crie um programa com uma função chamada
-# fatorial(), que receba dois parâmetros:
-# o primeiro será o número a calcular o
-# fatorial e o segundo será opcional e
-# lógico que indique se será exibido ou
-# não o processo de cálculo do fatorial. O
-# fatorial deve ser guardado num ficheiro
-# txt.
+
+#Crie um programa com uma função chamada fatorial(), que receba dois parâmetros: o primeiro será o número a calcular o fatorial e o segundo será opcional e lógico que indique se será exibido ou não o processo de cálculo do fatorial. O fatorial deve ser guardado num ficheiro txt.
 
 
 def fatorial(numero: int, mostra=False) -> str:
     """
-    -> Calculo de um numero com possibilidade de ver ym calculo
+    -> Calcula o fatorial de um número, com possibilidade de ver o cálculo.
     :param numero: Número a ser usado para calcular o fatorial
-    :param mostra:Opicional e boleada para True, ou false
+    :param mostra: Opcional e Booleada, para True (ver), False (não ver)
     :return: string
     """
+
     from math import factorial
     fatorial_calculado = factorial(numero)
+    calculo = ''
 
-    if mostra: # if mostra == True | if not mostra == False
+    if mostra: # if mostra == True | if not mostra: if mostra == False
         while numero >= 1:
             if numero == 1:
                 calculo += f'{numero} = '
             else:
                 calculo += f'{numero} x '
-                numero -= 1
+            numero -= 1
 
     calculo += str(fatorial_calculado)
 
@@ -34,10 +30,17 @@ def fatorial(numero: int, mostra=False) -> str:
 def guarda_txt(txt: str) -> None:
     from pathlib import Path
 
-    caminho = Path(r'factorial.txt')
+    caminho = Path(r'fatorial.txt')
 
-    with caminho.open('a')
+    with caminho.open('a', encoding='utf-8', errors='ignore') as file:
+        file.write(f' {txt}\n')
 
+    print('Fatorial criado com sucesso.')
+
+
+
+
+guarda_txt(fatorial(5,True))
 
 
 
